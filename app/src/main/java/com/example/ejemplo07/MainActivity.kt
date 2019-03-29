97181f854f757dcfc2d631e01212a2173257cfa8
Javier Martinez <00034315@uca.edu.sv>
2019-03-28 23:21:46 -0600

    Apararece listado  de pokemons, y ya es puede buscar por tipo

--- app/src/main/java/com/example/ejemplo07/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo07/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.ejemplo07
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
 import com.example.ejemplo07.Utilities.NetworkUtils
+import com.example.ejemplo08.models.Pokemon
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 import org.json.JSONArray
@@ -17,71 +22,97 @@ import java.io.IOException
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+    var flagSearchTipo=false
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    var pokemonMutableList: MutableList<Pokemon> = ArrayList()
+    lateinit var mETIdNombre: EditText
+    lateinit var mButton: Button
 
-    internal var mPokemonNumber: EditText = null
-    internal var mSearchButton: Button = null
-     lateinit var mResultText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        bindView()
-
-        mSearchButton.setOnClickListener { view ->
-            val pokemonNumber = mPokemonNumber.text.toString().trim { it <= ' ' }
-            if (pokemonNumber.isEmpty()) {
-                mResultText.setText(R.string.text_nothing_to_show)
-            } else {
-
-                FetchPokemonTask().execute(pokemonNumber)
-            }
+        mETIdNombre= findViewById(R.id.et_pokemon_number)
+        mButton= findViewById(R.id.bt_search_pokemon)
+        mButton.setOnClickListener{
+            flagSearchTipo=true
+            Log.v("flag", "Si llego 1")
+            FetchPokemonTask().execute()
         }
+        FetchPokemonTask().execute()
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-    }
 
-    internal fun bindView() {
-        mPokemonNumber = findViewById(R.id.et_pokemon_number)
-        mSearchButton = findViewById(R.id.bt_search_pokemon)
-        mResultText = findViewById(R.id.tv_result)
-    }
+    fun initRecycler() {
 
+
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = PokemonAdapter(pokemonMutableList, this)
+
+        rv_pokemon_list.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
+    }
 
     private inner class FetchPokemonTask : AsyncTask<String, Void, String>() {
+        override fun doInBackground(vararg params: String?): String {
+            //  var POKEMON_INFO="pokemon"
+            val pokeAPI = elegirBusqueda()
 
-        override fun doInBackground(vararg pokemonNumbers: String): String? {
-
-            if (pokemonNumbers.size == 0) {
-                return null
-            }
-
-            val ID = pokemonNumbers[0]
-
-            val pokeAPI = NetworkUtils.buildUrl(ID)
-
-            try {
-                return NetworkUtils.getResponseFromHttpUrl(pokeAPI!!)
+            return try {
+                Log.v("flag", "Hizo http "+pokeAPI)
+                NetworkUtils.getResponseFromHttpUrl(pokeAPI)!!
             } catch (e: IOException) {
                 e.printStackTrace()
-                return ""
+                ""
             }
+        }
+        fun elegirBusqueda() : URL {
+            if(!flagSearchTipo){
+                return NetworkUtils.buildUrl("pokemon")
+            }else{
+                Log.v("flag", "Entro a elegirBusqueda")
+                return NetworkUtils.buildUrl("type", mETIdNombre.text.toString())
 
+            }
         }
 
+
         override fun onPostExecute(pokemonInfo: String?) {
+
             if (pokemonInfo != null || pokemonInfo != "") {
-                var jsonPokemons=JSONObject(pokemonInfo)
-                var pokeArray = jsonPokemons.getJSONArray("pokemon")
 
-                var listaP = mutableListOf<String>()
+                var jsonPokeInfo= JSONObject(pokemonInfo)
+                var arrayPokemons: JSONArray
+                var pokeType: String?
+                pokemonMutableList.clear()
+
+                if(flagSearchTipo){
+                    pokeType= jsonPokeInfo.getString("name").toString()
+                    mETIdNombre.setText(pokeType)
+                    arrayPokemons= jsonPokeInfo.getJSONArray("pokemon")
+                    for(i in 0 until arrayPokemons.length()){
+
+                        var id= arrayPokemons.getJSONObject(i).getJSONObject("pokemon").getString("url").substring(34,(arrayPokemons.getJSONObject(i).getJSONObject("pokemon").getString("url").length-1))
+                        var nuevoPok= Pokemon(i, arrayPokemons.getJSONObject(i).getJSONObject("pokemon").getString("name"), arrayPokemons.getJSONObject(i).getJSONObject("pokemon").getString("url"),id)
+                        pokemonMutableList.add(nuevoPok)
+                    }
+                }else{
+                    arrayPokemons= jsonPokeInfo.getJSONArray("results")
+                    for(i in 0 until arrayPokemons.length()) {
+                        var id= arrayPokemons.getJSONObject(i).getString("url").substring(34,(arrayPokemons.getJSONObject(i).getString("url").length-1))
+                        var nuevoPok= Pokemon(i, arrayPokemons.getJSONObject(i).getString("name"), arrayPokemons.getJSONObject(i).getString("url"),id)
+                        pokemonMutableList.add(nuevoPok)
+
+                    }
+                }
 
 
-                for (i in 0..pokeArray.length()-1) listaP.add(i,pokeArray.getJSONObject(1).getJSONObject("pokemon").getString("name"))
-
-            } else {
+                initRecycler()
             }
         }
     }
